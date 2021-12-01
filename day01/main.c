1a28bcb41c5794f7dcdb59ef73f8afd2d3e7bcf6
Xavier Belanche <xbelanch@protonmail.com>
2021-12-01 20:46:23 +0100

    Add solution for part Two of Day 1

--- day01/main.c
+++ day01/main.c
@@ -2,17 +2,30 @@
 #include <stdlib.h>
 #define MAX_SIZE_NUMBER 4
 
-int depth[2001];
-int depth_size = 0;
+int arr[2048];
+int arr_size = 0;
 
 int partOne() {
     int increases = 0;
-    for (int i = 1; i < depth_size; ++i) {
-        if (depth[i - 1] < depth[i]) {
+    for (int i = 1; i < arr_size; ++i) {
+        if (arr[i - 1] < arr[i]) {
             increases++;
         }
     }
-    return increases;
+    return (increases);
+}
+
+int partTwo() {
+    int increases = 0;
+    // We don't count the last three numbers of the array
+    for (int i = 0; i < arr_size - (3); ++i) {
+        int curr = arr[i] + arr[i + 1] + arr[i + 2];
+        int next = arr[i + 1] + arr[i + 2] + arr[i + 3];
+        if (next > curr)
+            increases++;
+    }
+    fprintf(stdout, "arr_size: %d\n", arr_size);
+    return(increases);
 }
 
 int parse_input_file(char *filename)
@@ -21,21 +34,21 @@ int parse_input_file(char *filename)
     if (file == NULL) {
         fprintf(stderr, "Cannot open file: %s\n", filename);
         return (1);
-    }
 
+    }
     int c;
     int index = 0;
     char *number = malloc(sizeof(char) * MAX_SIZE_NUMBER + 1);
     while ((c = getc(file)) != EOF) {
         if (c == '\n') {
             number[index] = '\0';
-            depth[depth_size++] = atoi(number);
+            arr[arr_size++] = atoi(number);
             index = 0;
         } else {
             number[index++] = c;
         }
     }
-    depth[depth_size++] = atoi(number);
+    arr[arr_size] = atoi(number);
     fclose(file);
     return (0);
 }
@@ -45,8 +58,9 @@ int main(int argc, char *argv[])
     (void) argc;
     (void) argv[0];
     // parse_input_file("sample-01.txt");
-    parse_input_file("input-01.txt");
+    parse_input_file("input.txt");
     fprintf(stdout, "Solution part One: %d\n", partOne());
+    fprintf(stdout, "Solution part Two: %d\n", partTwo());
 
     return (0);
 }
