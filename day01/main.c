a9e91897d8a8625ad3df60f94374bd053ddd77f2
Xavier Belanche <xbelanch@protonmail.com>
2021-12-01 18:18:46 +0100

    Parse numbers and save them as array of integers

--- day01/main.c
+++ day01/main.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #define MAX_SIZE_NUMBER 4
 
+int depth[2000];
+int depth_size = 0;
+
+void print_depth_numbers() {
+    for (int i = 0; i < depth_size; ++i) {
+        fprintf(stdout, "%d\n", depth[i]);
+    }
+}
+
 int parse_input_file(char *filename)
 {
     FILE *file = fopen(filename, "rb");
@@ -15,17 +24,14 @@ int parse_input_file(char *filename)
     char *number = malloc(sizeof(char) * MAX_SIZE_NUMBER + 1);
     while ((c = getc(file)) != EOF) {
         if (c == '\n') {
-            fprintf(stdout, "%s", number);
             number[index] = '\0';
-            printf("-");
+            depth[depth_size++] = atoi(number);
             index = 0;
         } else {
             number[index++] = c;
         }
     }
-    // print last number
-    fprintf(stdout, "%s", number);
-
+    depth[depth_size++] = atoi(number);
     fclose(file);
     return (0);
 }
@@ -35,7 +41,8 @@ int main(int argc, char *argv[])
     (void) argc;
     (void) argv[0];
     parse_input_file("sample-01.txt");
-    parse_input_file("input-01.txt");
+    print_depth_numbers();
+    // parse_input_file("input-01.txt");
 
     return (0);
 }
