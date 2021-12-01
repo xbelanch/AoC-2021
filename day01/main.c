f168857f6e8392e7697e90bdd214fc3b991f5aae
Xavier Belanche <xbelanch@protonmail.com>
2021-12-01 18:35:47 +0100

    Add solution for part one of Day 1

--- day01/main.c
+++ day01/main.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #define MAX_SIZE_NUMBER 4
 
-int depth[2000];
+int depth[2001];
 int depth_size = 0;
 
-void print_depth_numbers() {
-    for (int i = 0; i < depth_size; ++i) {
-        fprintf(stdout, "%d\n", depth[i]);
+int partOne() {
+    int increases = 0;
+    for (int i = 1; i < depth_size; ++i) {
+        if (depth[i - 1] < depth[i]) {
+            increases++;
+        }
     }
+    return increases;
 }
 
 int parse_input_file(char *filename)
@@ -40,9 +44,9 @@ int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv[0];
-    parse_input_file("sample-01.txt");
-    print_depth_numbers();
-    // parse_input_file("input-01.txt");
+    // parse_input_file("sample-01.txt");
+    parse_input_file("input-01.txt");
+    fprintf(stdout, "Solution part One: %d\n", partOne());
 
     return (0);
 }
