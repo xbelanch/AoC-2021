b03ad817a9aaf624182181445fff27b0b42c43b1
Xavier Belanche <xbelanch@protonmail.com>
2021-12-12 17:18:57 +0100

    Tweaking the dummy input parser d04e02
    Signed-off-by: Xavier Belanche <xbelanch@protonmail.com>

--- day04/main.c
+++ day04/main.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SIZE_LINE 1024
-#define MAX_SIZE_BOARDS 128
+#define MAX_SIZE_LINE 2048
+#define MAX_SIZE_BOARDS 512
+#define SIZE_DRAW_NUMBERS 128
+#define BOARD_ROW 5
+#define BOARD_COL 5
 #define SAMPLE_INPUT "sample-01.txt"
 #define INPUT_FILE "input.txt"
 
 char *draw_numbers;
+size_t draw_numbers_size;
 char *board[MAX_SIZE_BOARDS];
-size_t board_size = 0;
+size_t board_size;
 
 void dumpBoards() {
     for (size_t i = 0; i < board_size; ++i) {
@@ -17,18 +21,49 @@ void dumpBoards() {
     }
 }
 
+size_t *parseNumbers(char *input, char delimiter) {
+    size_t len = strlen(input);
+    char number[2];
+    size_t j = 0;
+    size_t count = 0;
+    size_t *numbers = malloc(sizeof(size_t) * draw_numbers_size);
+    char prev = delimiter;
+    for (size_t i = 0; i < len; ++i) {
+        if (input[i] == delimiter && prev == delimiter) {
+            prev = delimiter;
+            continue;
+        } else if (input[i] == delimiter && prev != delimiter) {
+            numbers[count++] = atol(number);
+            memset(&number, 0, 2);
+            prev = delimiter;
+            j = 0;
+        } else {
+            number[j++] = input[i];
+            prev = input[i];
+        }
+    }
+
+    numbers[count] = atol(number);
+    return numbers;
+}
+
 size_t partOne(char *input) {
     size_t solution = 0;
     FILE *fp = fopen(input, "rb");
-    if (fp == NULL)
+    if (fp == NULL) {
+        fprintf(stderr, "%s not found\n", input);
         return(1);
+    }
 
     char *line = malloc(sizeof(char) * MAX_SIZE_LINE);
     size_t number_line = 0;
     int count = 0;
 
+    draw_numbers_size = 0;
+    board_size = 0;
     board[board_size] = malloc(sizeof(char) * MAX_SIZE_LINE);
-    // end = board[board_size];
+    char *tmp;
+
     while (fgets(line, MAX_SIZE_LINE, fp)) {
 
         if (line[0] == '\n')
@@ -39,33 +74,57 @@ size_t partOne(char *input) {
             line[strlen(line) - 1] = '\0'; // remove newline
             size_t len = strlen(line);
             draw_numbers = malloc(sizeof(char) * len);
+            tmp = (char*)malloc(sizeof(char) * len);
             memcpy(draw_numbers, line, len);
-            printf("%s\n", draw_numbers);
-        } else
+            // get size of draw random numbers
+            memcpy(tmp, line, len);
+            tmp = strtok(line, ",");
+            while (tmp != NULL) {
+                tmp = strtok(NULL, ",");
+                draw_numbers_size++;
+            }
+            free(tmp);
+            printf("draw_numbers_size: %lu\n", draw_numbers_size);
+        }
+        // else
 
         // get boards
-        {
-            if (line[strlen(line) - 1] == '\n')
-                line[strlen(line) - 1] = '\0';
+        // {
+        //     if (line[strlen(line) - 1] == '\n')
+        //         line[strlen(line) - 1] = '\0';
 
-            size_t len = strlen(line);
-            size_t len2 = strlen(board[board_size]);
-            char *end = &board[board_size][len2];
-            memcpy(end, line, len);
-            end = &board[board_size][len2];
-            memcpy(end, " ", 1);
-            count++;
-            if (count % 5 == 0) {
-                count = 0;
-                board_size++;
-                board[board_size] = malloc(sizeof(char) * MAX_SIZE_LINE);
-            }
-        }
+        //     size_t len = strlen(line);
+        //     size_t len2 = strlen(board[board_size]);
+        //     char *end = &board[board_size][len2] + 1;
+        //     memcpy(end, line, len);
+        //     end = &board[board_size][len2];
+        //     memcpy(end, " ", 1);
+        //     count++;
+        //     if (count % 5 == 0) {
+        //         count = 0;
+        //         board_size++;
+        //         board[board_size] = malloc(sizeof(char) * MAX_SIZE_LINE);
+        //     }
+        // }
 
         number_line++;
     }
 
-    dumpBoards();
+    // Dump random draw numbers
+    size_t *dw = parseNumbers(draw_numbers, ',');
+    for (size_t i = 0; i < draw_numbers_size; ++i) {
+        printf("%lu,", dw[i]);
+    }
+    putchar('\n');
+    // Dump board data
+    // for (size_t j = 0; j < board_size; ++j) {
+    //     size_t *bd = parseNumbers(board[j], ' ');
+    //     for (size_t i = 0; i < BOARD_COL * BOARD_ROW; ++i) {
+    //         printf("%lu,", bd[i]);
+    //     }
+    //     putchar('\n');
+    // }
+
 
     fclose(fp);
     return(solution);
@@ -75,7 +134,8 @@ int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv[0];
-    printf("Solution for the part One of Day 4 (%s): %lu", SAMPLE_INPUT, partOne(SAMPLE_INPUT));
+    printf("Solution for the part One of Day 4 (%s): %lu\n", SAMPLE_INPUT, partOne(SAMPLE_INPUT));
+    printf("Solution for the part One of Day 4 (%s): %lu\n", INPUT_FILE, partOne(INPUT_FILE));
 
     return (0);
 }
