ec6f4062a01c8a157e6e2e341efd6370c7f03399
Xavier Belanche <xbelanch@protonmail.com>
2021-12-12 10:23:07 +0100

    Rewrite part Two of Day 3 e03

--- day03/main.c
+++ day03/main.c
@@ -3,225 +3,6 @@
 #include <string.h>
 #include <math.h>
 
-// unsigned long long partOne(char *input) {
-
-//     FILE *fp = fopen(input, "rb");
-//     if (NULL == fp) {
-//         fprintf(stderr, "Cannot open file %s\n", input);
-//     }
-
-//     char c = getc(fp);
-//     char *line[1024];
-//     size_t line_size = 0;
-//     line[line_size] = malloc(sizeof(char) * 512);
-//     size_t len = 0;
-//     while (c != EOF) {
-//         if (c == '\n') {
-//             // putchar('\n');
-//             line[line_size++][len] = '\0';
-//             line[line_size] = malloc(sizeof(char) * 512);
-//             len = 0;
-//         } else {
-//             // putchar(c);
-//             line[line_size][len++] = c;
-//         }
-//         c = getc(fp);
-//     }
-//     line[line_size][len] = '\0';
-//     printf("line size: %ld\n", line_size);
-//     fclose(fp);
-
-//     char *binary_number[line_size];
-//     for (size_t j = 0; j < strlen(line[0]); ++j) {
-//         binary_number[j] = malloc(sizeof(char) * 1024);
-//         for (size_t i = 0; i < line_size; ++i) {
-//             binary_number[j][i] = line[i][j];
-//         }
-//         binary_number[j][line_size] = '\0';
-//     }
-
-//     int count_one = 0;
-//     int count_zero = 0;
-//     char *gamma_rate = malloc(sizeof(char) * strlen(line[0]));
-
-//     for (size_t j = 0; j < strlen(line[0]); ++j) {
-//         for (size_t i = 0; i < strlen(binary_number[j]); ++i)
-//             binary_number[j][i] == '1' ? count_one++ : count_zero++;
-
-//         if (count_one > count_zero) {
-//             gamma_rate[j] = '1';
-//         } else {
-//             gamma_rate[j] = '0';
-//         }
-//         count_one = 0;
-//         count_zero = 0;
-//     }
-//     gamma_rate[strlen(line[0])] = '\0';
-//     // printf("gamma rate in binary: %s\n", gamma_rate);
-//     int gamma_rate_base10 = convert(atoll(gamma_rate));
-//     // printf("gamma rate in decimal: %d\n", gamma_rate_base10);
-
-//     char *epsilon_rate = malloc(sizeof(char) * strlen(gamma_rate));
-//     for (size_t i = 0; i < strlen(gamma_rate); ++i) {
-//         if (gamma_rate[i] == '1')  {
-//             epsilon_rate[i] = '0';
-//         } else {
-//             epsilon_rate[i] = '1';
-//         }
-//     }
-//     epsilon_rate[strlen(line[0])] = '\0';
-//     // printf("epsilon rate in binary: %s\n", epsilon_rate);
-//     int epsilon_rate_base10 = convert(atoll(epsilon_rate));
-//     // printf("epsilon rate in decimal: %d\n", epsilon_rate_base10);
-
-//     return (gamma_rate_base10 * epsilon_rate_base10);
-// }
-
-// size_t partTwo(char *input) {
-//     FILE *fp = fopen(input, "rb");
-//     if (NULL == fp) {
-//         fprintf(stderr, "Cannot open file %s\n", input);
-//     }
-
-//     char c = getc(fp);
-//     char *line[1024];
-//     size_t line_size = 0;
-//     line[line_size] = malloc(sizeof(char) * 512);
-//     size_t len = 0;
-//     while (c != EOF) {
-//         if (c == '\n') {
-//             line[line_size++][len] = '\0';
-//             line[line_size] = malloc(sizeof(char) * 512);
-//             len = 0;
-//         } else {
-//             line[line_size][len++] = c;
-//         }
-//         c = getc(fp);
-//     }
-//     line[line_size][len] = '\0';
-//     fclose(fp);
-
-//     size_t stack_size = line_size - 1;
-//     size_t *stack = malloc(sizeof(size_t) * stack_size);
-
-//     unsigned int max_bits = strlen(line[0]) - 1;
-//     for (size_t  i = 0; i < stack_size; ++i) {
-//         stack[i] = convert(atoll(line[i]));
-//     }
-
-//     size_t bit[2] = {0, 0};
-//     size_t bitMax = 1;
-
-//     size_t *tmp = NULL;
-
-//     for (int i = max_bits; i >= 0; --i) {
-//         for (size_t j = 0; j < stack_size; ++j) {
-//             if (getBitValue(stack[j], i)) {
-//                 bit[1] += 1;
-//             } else {
-//                 bit[0] += 1;
-//             }
-//         }
-
-//         if (bit[0] > bit[1]) bitMax = 0;
-
-//         for (size_t l = 0; l < stack_size; ++l) {
-//             if (getBitValue(stack[l], i) != bitMax) {
-//                 for (size_t k = l; k < stack_size; ++k) {
-//                     stack[k] = stack[k + 1];
-//                 }
-//                 stack_size--;
-//                 tmp = realloc(stack, stack_size * sizeof(size_t));
-//                 stack = tmp;
-//                 tmp = NULL;
-
-//                 l--;
-//                 if (l > stack_size)
-//                     l = 0;
-//             }
-//         }
-//         bitMax = 1;
-//         bit[0] = 0; bit[1] = 0;
-
-//         if (stack_size == 1)
-//             break;
-//     }
-
-//     // Otherwise, repeat the process, considering the next bit to the right.
-//     printf("stack_size: %lu | max_bits: %u\n", stack_size, max_bits);
-//     size_t oxygen_generator_rating = 0;
-//     if (stack_size > 1) {
-//         // TODO: Fix this stupid hack
-//         oxygen_generator_rating = stack[0] - 9;
-//     } else {
-//         oxygen_generator_rating = stack[0];
-//     }
-
-//     printf("ox: %lu expected  (%s)\n", oxygen_generator_rating, input);
-
-//     // TODO: Rewrite this as a function
-//     stack_size = line_size - 1;
-//     stack = malloc(sizeof(size_t) * stack_size);
-//     for (size_t  i = 0; i < stack_size; ++i) {
-//         stack[i] = convert(atoll(line[i]));
-//     }
-
-//     bit[0] = 0; bit[1] = 0;
-//     bitMax = 0;
-//     tmp = NULL;
-
-//     for (int i = max_bits; i >= 0; --i) {
-//         for (size_t j = 0; j < stack_size; ++j) {
-//             if (getBitValue(stack[j], i)) {
-//                 bit[1] += 1;
-//             } else {
-//                 bit[0] += 1;
-//             }
-//         }
-
-//         if (bit[1] < bit[0]) bitMax = 1;
-
-//         for (size_t l = 0; l < stack_size; ++l) {
-//             if (getBitValue(stack[l], i) != bitMax) {
-//                 for (size_t k = l; k < stack_size; ++k) {
-//                     stack[k] = stack[k + 1];
-//                 }
-//                 stack_size--;
-//                 tmp = realloc(stack, stack_size * sizeof(size_t));
-//                 stack = tmp;
-//                 tmp = NULL;
-
-//                 l--;
-//                 if (l > stack_size)
-//                     l = 0;
-//             }
-//         }
-//         bitMax = 0;
-//         bit[0] = 0; bit[1] = 0;
-
-//         if (stack_size == 1)
-//             break;
-//     }
-
-//     // Otherwise, repeat the process, considering the next bit to the right.
-//     printf("stack_size: %lu | max_bits: %u\n", stack_size, max_bits);
-//     size_t co2_scrubber_rating = 0;
-//     if (stack_size > 1) {
-//         co2_scrubber_rating = stack[0] - ((stack[1] - stack[0]) / 2);
-//     } else {
-//         co2_scrubber_rating = stack[0];
-//     }
-
-//     printf("co2: %lu expected  (%s)\n", co2_scrubber_rating, input);
-
-//     return (oxygen_generator_rating * co2_scrubber_rating);
-// }
-
-
-#define SAMPLE_SIZE 12
-#define SAMPLE_BITS 5
-#define INPUT_SIZE 1000
-#define INPUT_BITS 12
 
 int bin2dec(int n) {
   int dec = 0, i = 0, rem;
@@ -234,8 +15,131 @@ int bin2dec(int n) {
   return (dec);
 }
 
-unsigned int getBitValue(int value, int bitPosition) {
+void dec2bin(size_t c)
+{
+   int i = 0;
+   for(i = 31; i >= 0; i--){
+     if((c & (1 << i)) != 0){
+       printf("1");
+     }else{
+       printf("0");
+     }
+   }
+   putchar('\n');
+}
+
+// TODO: Rewrite this shit from scratch
+unsigned long long partOne(char *input) {
+    FILE *fp = fopen(input, "rb");
+    if (NULL == fp) {
+        fprintf(stderr, "Cannot open file %s\n", input);
+    }
+
+    char c = getc(fp);
+    char *line[1024];
+    size_t line_size = 0;
+    line[line_size] = malloc(sizeof(char) * 512);
+    size_t len = 0;
+    while (c != EOF) {
+        if (c == '\n') {
+            // putchar('\n');
+            line[line_size++][len] = '\0';
+            line[line_size] = malloc(sizeof(char) * 512);
+            len = 0;
+        } else {
+            // putchar(c);
+            line[line_size][len++] = c;
+        }
+        c = getc(fp);
+    }
+    line[line_size][len] = '\0';
+    printf("line size: %ld\n", line_size);
+    fclose(fp);
+
+    char *binary_number[line_size];
+    for (size_t j = 0; j < strlen(line[0]); ++j) {
+        binary_number[j] = malloc(sizeof(char) * 1024);
+        for (size_t i = 0; i < line_size; ++i) {
+            binary_number[j][i] = line[i][j];
+        }
+        binary_number[j][line_size] = '\0';
+    }
+
+    int count_one = 0;
+    int count_zero = 0;
+    char *gamma_rate = malloc(sizeof(char) * strlen(line[0]));
+
+    for (size_t j = 0; j < strlen(line[0]); ++j) {
+        for (size_t i = 0; i < strlen(binary_number[j]); ++i)
+            binary_number[j][i] == '1' ? count_one++ : count_zero++;
+
+        if (count_one > count_zero) {
+            gamma_rate[j] = '1';
+        } else {
+            gamma_rate[j] = '0';
+        }
+        count_one = 0;
+        count_zero = 0;
+    }
+    gamma_rate[strlen(line[0])] = '\0';
+    // printf("gamma rate in binary: %s\n", gamma_rate);
+    int gamma_rate_base10 = bin2dec(atoll(gamma_rate));
+    // printf("gamma rate in decimal: %d\n", gamma_rate_base10);
+
+    char *epsilon_rate = malloc(sizeof(char) * strlen(gamma_rate));
+    for (size_t i = 0; i < strlen(gamma_rate); ++i) {
+        if (gamma_rate[i] == '1')  {
+            epsilon_rate[i] = '0';
+        } else {
+            epsilon_rate[i] = '1';
+        }
+    }
+    epsilon_rate[strlen(line[0])] = '\0';
+    // printf("epsilon rate in binary: %s\n", epsilon_rate);
+    int epsilon_rate_base10 = bin2dec(atoll(epsilon_rate));
+    // printf("epsilon rate in decimal: %d\n", epsilon_rate_base10);
+
+    return (gamma_rate_base10 * epsilon_rate_base10);
+}
+
+#define SAMPLE_SIZE 12
+#define SAMPLE_BITS 5
+#define INPUT_SIZE 1000
+#define INPUT_BITS 12
+#define TRUE 1
+#define FALSE 0
+
+unsigned int getBit(int value, int bitPosition) {
     return((value >> bitPosition) & 1);
+}
+
+unsigned int getCommonBit(size_t *arr, size_t size, int bitPosition, int bitCriteria) {
+    unsigned bit[2] = {0, 0};
+    for (size_t i = 0; i < size; ++i) {
+        if (getBit(arr[i], bitPosition)) { // 1
+            bit[1]++;
+        } else {
+            bit[0]++;
+        }
+    }
+
+    if (bitCriteria == 1) {
+        if (bit[1] > bit[0] || bit[1] ==  bit[0]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    if (bitCriteria == 0) {
+        if (bit[1] > bit[0] || bit[1] ==  bit[0]) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    return (99);
 }
 
 int values[SAMPLE_SIZE];
@@ -255,16 +159,115 @@ size_t readInputFile(char *input) {
     return (0);
 }
 
-void dumpValues() {
-    for (int i = 0; i < idx_val; ++i) {
-        printf("%d,", values[i]);
+void dumpValues(size_t *values, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        dec2bin(values[i]);
     }
 }
 
-int partTwo() {
+size_t getOGR(size_t *bit_criteria, size_t left, int checkBit)
+    {
+        size_t ogr = 0;
+        size_t *tmp;
+        while (left > 1) {
+
+            // set 1 for OGR
+            unsigned int commonBit = getCommonBit(bit_criteria, left, checkBit, 1);
+            // printf("common bit: %d\n", commonBit);
+
+            size_t tmp_size = 0;
+            for (size_t i = 0; i < left; ++i) {
+                if (getBit(bit_criteria[i], checkBit) != commonBit) {
+                    bit_criteria[i] = FALSE;
+                } else {
+                    tmp_size++;
+                }
+            }
+
+            // printf("tmp_size: %lu\n", tmp_size);
+            tmp = malloc(sizeof(size_t) * tmp_size);
+            int j = 0;
+            for (size_t i = 0; i < left; ++i) {
+                if (bit_criteria[i] != FALSE) {
+                    tmp[j++] = bit_criteria[i];
+                }
+            }
+
+            left = tmp_size;
+            bit_criteria = tmp;
+            // dumpValues(bit_criteria, left);
+            checkBit--;
+            if (checkBit < 0)
+                checkBit = SAMPLE_BITS - 1;
+
+            if (left == 1)
+                ogr = bit_criteria[left - 1];
+        }
+        return ogr;
+    } // end of while
+
+size_t getCO2(size_t *bit_criteria, size_t left, int checkBit)
+    {
+        size_t co2 = 0;
+        size_t *tmp;
+        while (left > 1) {
+
+            // set 0 for CO2
+            unsigned int commonBit = getCommonBit(bit_criteria, left, checkBit, 0);
+            // printf("common bit: %d\n", commonBit);
+
+            size_t tmp_size = 0;
+            for (size_t i = 0; i < left; ++i) {
+                if (getBit(bit_criteria[i], checkBit) != commonBit) {
+                    bit_criteria[i] = FALSE;
+                } else {
+                    tmp_size++;
+                }
+            }
+
+            // printf("tmp_size: %lu\n", tmp_size);
+            tmp = malloc(sizeof(size_t) * tmp_size);
+            int j = 0;
+            for (size_t i = 0; i < left; ++i) {
+                if (bit_criteria[i] != FALSE) {
+                    tmp[j++] = bit_criteria[i];
+                }
+            }
+
+            left = tmp_size;
+            bit_criteria = tmp;
+            // dumpValues(bit_criteria, left);
+            checkBit--;
+            if (checkBit < 0)
+                checkBit = SAMPLE_BITS - 1;
+
+            if (left == 1)
+                co2 = bit_criteria[left - 1];
+        }
+        return co2;
+    } // end of while
+
+size_t partTwo() {
     readInputFile("sample-01.txt");
-    dumpValues();
-    return (0);
+
+    size_t ogr = 0;
+    size_t co2 = 0;
+
+    size_t *bit_criteria = malloc(sizeof(size_t) * SAMPLE_SIZE);
+    for (size_t i = 0; i < SAMPLE_SIZE; ++i) {
+        bit_criteria[i] = values[i];
+    }
+
+    ogr = getOGR(bit_criteria, SAMPLE_SIZE, SAMPLE_BITS - 1);
+
+    bit_criteria = malloc(sizeof(size_t) * SAMPLE_SIZE);
+    for (size_t i = 0; i < SAMPLE_SIZE; ++i) {
+        bit_criteria[i] = values[i];
+    }
+
+    co2 = getCO2(bit_criteria, SAMPLE_SIZE, SAMPLE_BITS - 1);
+
+    return (ogr * co2);
 }
 
 int main(int argc, char *argv[])
@@ -272,7 +275,7 @@ int main(int argc, char *argv[])
     (void) argc;
     (void) argv[0];
 
-    partTwo();
+    printf("Solution for part Two of Day 3: %lu", partTwo());
 
     return (0);
 }
